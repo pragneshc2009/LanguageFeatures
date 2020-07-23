6cc4a24d056919fe940d164651fb18875a5f2831
pragneshc2009 <32779701+pragneshc2009@users.noreply.github.com>
2020-07-23 17:25:13 -0500

    Add project files.

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        bool FilterByPrice(Product p)
+        {
+            return (p?.Price ?? 0) >= 20;
+        }
+
+        public ViewResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = Product.GetProducts()
+            };
+            Product[] productArray =
+            {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M},
+                new Product { Name = "Soccer Ball", Price = 19.50M},
+                new Product { Name = "Corner Flag", Price = 34.95M}
+            };
+            //decimal priceFilterTotal = productArray.FilterByPrice(20).TotalPrices();
+            //decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
+            Func<Product, bool> nameFilter = delegate (Product prod)
+            {
+                return prod?.Name?[0] == 'S';
+            };
+
+            decimal priceFilterTotal = productArray.Filter(FilterByPrice).TotalPrices();
+            decimal nameFilterTotal = productArray.Filter(nameFilter).TotalPrices();
+            return View("Index", new string[] {  $"Price Total: {priceFilterTotal:C2}", $"Name Total: {nameFilterTotal:C2}" });
+        }
+    }
+}
